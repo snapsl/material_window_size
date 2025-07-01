35d1f322b4ac6252ce98e77d08e6a113707d45b4
snapsl <snapsl@users.noreply.github.com>
2025-07-01 11:16:15 +0200

    add side sheet width constant

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
 
-/// BoxConstraints matching the material3 guidelines
+/// [BoxConstraints] matching the material3 guidelines
 const BoxConstraints materialBoxConstraints = BoxConstraints(maxWidth: 840);
+
+/// Width for a standard side sheet.
+///
+/// https://m3.material.io/components/side-sheets
+const double standardSideSheetWidth = 400;
